148102970d6cdbf574b2eaeb4acd4e5f62510168
nezih <m.nezihkaya@gmail.com>
2023-03-10 11:17:27 -0500

    BasePage added

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+import java.util.List;
+
+public class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//ul[@id='appmenu']//*[@viewBox='0 0 20 20']/following-sibling::span") public List<WebElement> mainModules;
+    @FindBy (xpath = "//a[@aria-controls='header-menu-unified-search']") public WebElement searchBtn;
+    @FindBy (xpath = "//input[@type='search']") public WebElement searchInput;
+}
