9273639b7b217803945357101d8acc927a1a5e96
nezih <m.nezihkaya@gmail.com>
2023-03-10 10:51:47 -0500

    WebTableUtils added

---
+++ src/test/java/utilities/WebTableUtils.java
@@ -0,0 +1,16 @@
+package utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebTableUtils {
+    public static List<String> getHeadersFromVehicleModelTable() {
+        List<WebElement> headerElements = Driver.getDriver().findElements(By.xpath("//thead[@class='grid-header']//th//span[1]"));
+        List<String> headerTitles = new ArrayList<>();
+        headerElements.forEach(k -> headerTitles.add(k.getText()));
+        return headerTitles;
+    }
+}
