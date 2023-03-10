2fb093dcf4843428d543471c6ae1af4c21dc33dd
nezih <m.nezihkaya@gmail.com>
2023-03-10 11:18:36 -0500

    LoginPage added

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class LoginPage {
+    public LoginPage (){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy (id = "user") public WebElement usernameInput;
+    @FindBy (id = "password") public WebElement passwordInput;
+    @FindBy (id = "submit-form") public WebElement loginBtn;
+    @FindBy (xpath = "//p[@class='warning wrongPasswordMsg']") public WebElement warningMessage;
+    @FindBy (xpath = "//div[@class='logo logo-icon']") public WebElement mainLogo;
+}
