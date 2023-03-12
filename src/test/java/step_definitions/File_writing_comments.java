40f8ba0f380b42f08baecda55bdfc80c05f6f432
nezih <m.nezihkaya@gmail.com>
2023-03-11 20:39:45 -0500

    File_writing_comments and feature file created

---
+++ src/test/java/step_definitions/File_writing_comments.java
@@ -0,0 +1,18 @@
+package step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+public class File_writing_comments {
+    @And("user write a {string} comment inside the input box")
+    public void userWriteACommentInsideTheInputBox(String arg0) {
+    }
+
+    @And("user click the submit button to post it")
+    public void userClickTheSubmitButtonToPostIt() {
+    }
+
+    @Then("Verify the {string} comment is displayed in the comment section")
+    public void verifyTheCommentIsDisplayedInTheCommentSection(String arg0) {
+    }
+}
